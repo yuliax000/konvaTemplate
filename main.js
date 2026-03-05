c26c6b9fcff079f51abc47b3a1e8363bed77ca63
yuliax000 <ArthyerX@gmail.com>
2026-03-05 16:20:14 +1100

    add stage and circle

---
+++ main.js
@@ -0,0 +1,19 @@
+// create the konva stage
+const stage = new Konva.Stage({
+  container: "konva-stage",
+  width: 500,
+  height: 500,
+  fill: "red",
+});
+
+const firstLayer = new Konva.Layer();
+const circle = new Konva.Circle({
+  x: 250,
+  y: 250,
+  radius: 50,
+  fill: "red",
+});
+
+firstLayerlayer.add(circle);
+
+stage.add(firstLayer);
